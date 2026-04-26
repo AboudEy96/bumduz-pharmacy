706bf3af157794dd28de761fa259d5ca02f80599
Abdulkadir <88458597+AboudEy96@users.noreply.github.com>
2026-04-26 22:38:49 +0300

    little fix

--- lib/service/UserService.dart
+++ lib/service/UserService.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:pharmacy/models/User/user.dart';
+import 'package:pharmacy/models/User/User.dart';
 
 class UserService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
